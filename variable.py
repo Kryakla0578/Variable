ab232122192a63a2acd06dd5199756d6e3a18f3b
Kryakla0578 <167692954+Kryakla0578@users.noreply.github.com>
2024-08-06 14:07:00 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+How_much_homework = 12
+Times_to_do = 1.5
+Name_of_course = "Python"
+Time_for_one = "hours"
+print(Name_of_course,", всего задач:",How_much_homework,", затрачено часов:",Times_to_do,", среднее время выполнения:",Times_to_do/How_much_homework,Time_for_one)
